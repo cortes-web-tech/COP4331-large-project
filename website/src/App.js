70441ba871366b472b3dbc3a45be5a7f6a08d863
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-02-22 19:14:52 -0500

    Basic page with interactive routes

--- website/src/App.js
+++ website/src/App.js
@@ -2,23 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Background from './background.png';
+import Header from './components/layout/Header';
+
+// Linked pages
+import Authors from './components/pages/Authors';
+import Demo from './components/pages/Demo';
+import Contact from './components/pages/Contact';
+import Start from './components/pages/Start';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
 
+
+      <Router>
+
       <div className="App">
-        <div style={sectionStyle}>
+
+        <Header/>
+
+
+
+        <div style = {sectionStyle} className="container">
+          <Route exact path="/start" component={Start}/>
+          <Route exact path="/authors" component={Authors}/>
+          <Route exact path="/demo" component={Demo}/>
+          <Route exact path="/contact" component={Contact}/>
 
         </div>
       </div>
+
+      </Router>
     );
   }
 }
 
 var sectionStyle = {
-  flex: 1,
+  flex: '1',
   width: "100%",
+  color: "black",
+  fontSize: "50px",
   height: "738px",
   backgroundImage: "url(" + Background +")"
 }
--- website/src/components/layout/Header.js
+++ website/src/components/layout/Header.js
@@ -1,24 +1,33 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 function Header(){
   return(
     <header style={headerStyle}>
-    <h1>Keepu.</h1>
-
+    <h1><Link style={fontStyle} to="/">Keepu.</Link></h1>
+     <Link style={linkStyle} to="/start">Get Started</Link> | <Link style={linkStyle} to="/authors">Our Authors</Link> | <Link style={linkStyle} to="/demo">Schedule A Demo</Link> | <Link style={linkStyle} to="/contact">Contact Us</Link>
     </header>
   )
 }
 
 const linkStyle = {
-  color: '#fff',
+  color: 'white',
   textDecoration: 'none'
 }
 
 const headerStyle = {
-  background: '#333',
-  color: '#fff',
+  background: '#d6b9e8',
+  height: "100px",
+  color: '#ffffff',
   textAlign: 'center',
-  padding: '10px'
+  padding: '10px',
+
 }
+
+const fontStyle = {
+  color: 'white'
+}
+
+
+
 export default Header
