68926dcf43b1c4a2bc027320d4559815b3f56795
Selena Castrolopez <selenacl22@gmail.com>
2019-04-09 15:00:08 -0400

    directory changes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('./db/mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const userRouter = require('./routers/user');
+const knot1Router = require('./routers/knot1');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+
+app.use(express.json());
+app.use(userRouter);
+app.use(knot1Router);
+app.use(cors({credentials: true, origin: true}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
+});
